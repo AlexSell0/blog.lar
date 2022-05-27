311e294e79029dfebb7a8e58123d995ab939a146
AlexSell0 <alexsell72@gmail.com>
2022-05-27 22:57:18 +0300

    Админка для блога

--- resources/views/admin/includs/sidebar.blade.php
+++ resources/views/admin/includs/sidebar.blade.php
@@ -2,6 +2,14 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Alexander Pierce</a>
+            </div>
+        </div>
         <li class="nav-item">
             <a href="{{ route('admin.user.index') }}" class="nav-link">
                 <i class="nav-icon far fa-user"></i>
