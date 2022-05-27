cbe3825aa9850471d52fb16ffe454a3c78945f17
AlexSell0 <alexsell72@gmail.com>
2022-05-27 23:03:06 +0300

    d

--- resources/views/admin/includs/sidebar.blade.php
+++ resources/views/admin/includs/sidebar.blade.php
@@ -4,10 +4,10 @@
              with font-awesome or any other icon font library -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ url('storage/images/avatar.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{ Auth::user()->name }}</a>
             </div>
         </div>
         <li class="nav-item">
